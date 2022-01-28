1e84fa38afa4da5db9204c2a56b28c30d279369b
Mandrijauskaite <97032997+Mandrijauskaite@users.noreply.github.com>
2022-01-28 11:02:05 +0200

    Uzbaigtas

---
+++ README.md
@@ -0,0 +1,26 @@
+![LICENSE](https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)
+![Security Status](https://img.shields.io/security-headers?label=Security&url=https%3A%2F%2Fgithub.com&style=flat-square)
+![Gluten Status](https://img.shields.io/badge/Gluten-Free-green.svg)
+![Eco Status](https://img.shields.io/badge/ECO-Friendly-green.svg)
+
+# Mergaite
+
+_33 group website project_
+
+This project is for educational porpuses only. Pull request are welcome, but priority for project authors! Thank you for your cooperation!
+
+Site published at: https://github.com/Mandrijauskaite/6-savaite-praktine-uzduotis
+
+Design: [Girl]( https://cdn.discordapp.com/attachments/850245533838868480/850246211415834634/unknown.png)
+
+## Project features
+
+-   Github pages
+-   responsive design (12 column grid system)
+-   fontawesome icons
+-   CSS animations
+-   CSS import
+
+## Authors
+
+Alina: [Github](https://github.com/Mandrijauskaite/3-mergaite)
---
+++ js/01-uzduotis.js
@@ -0,0 +1,32 @@
+console.log('------------');
+// Palyginti du skaičius a ir b. Išvesti į konsolę, kuris didesnis arba jie lygūs. (4 taškai)
+
+const a = 3;
+const b = 5;
+
+if (a <= b) {
+console.log('daugiau arba lygu');
+} else {
+    console.log('nei daugiau, nei lygu -> maziau');
+}
+console.log('------------');
+// Naudojant ciklą, išvesti į konsolę skaičius nuo 1 iki 10. (5 taškai)
+var skaiciai = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(skaiciai.length);
+
+console.log('------------');
+// Naudojant ciklą, išvesti į konsolę skaičius nuo 0, 2, 4, 6, 8, 10. (5 taškai)
+for (let i = 0; skaiciai.length - 1) {
+    console.log(skaiciai);
+}
+
+// Naudojant ciklą, sugeneruoti penkis atsitiktinius skaičius nuo 1 iki 10. Išvesti juos konsolėje. (5 taškai)
+// Naudojant ciklą, spausdinti atsitiktinius skaičius nuo 1 iki 10. Paskutinis atspausdintas skaičius turi būti 5. (7 taškai)
+// Sukurti masyvą, kurio ilgis būtų nuo 20 iki 30, o reikšmės būtų skaičiai nuo 10 iki 30. Surasti antrą didžiausią masyvo reikšmę, nenaudojant sort funkcijos. (7 taškai)
+// Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 100. Suskaičiuokite kiek yra kiekvienos raidės. (7 taškai)
+// Parašyti funkciją - lygineSuma. Funkcijos parametrai - du kintamieji. Testų reikalavimai - abu kitamieji turi būti arba skaičiai arba masyvai(negali būti vienas skaičius, kitas masyvas).
+// Jei kintamieji skaičiai, grąžinti skaičių sumą, jei kintamieji masyvai - grąžinti masyvų ilgių sumą. Jei suma nelyginė - grąžinti tekstą, kad suma nelyginė. (10 taškų)
+// Parašyti funkciją pirminisSkaicius. Funkcija turi vieną kintamąjį. Turi būti patikrinimas, kad kintamasis yra skaičius. Funkcija turi grąžinti ar pateiktas skaičius yra pirminis( pirminis skaičius yra tas, kuris dalinasi tik iš savęs ir tik iš vieneto be liekanos.) (10 taškų)
+// Parašyti funkciją telefonoNumeris. Funkcija turi priimti vieną kintamąjį - masyvą. Masyvo elementai - skaičiai, ilgis - 10. Funkcija turi grąžinti telefono numerį tokiu formatu - "(XXX) XXX-XXXX". (10 taškų)
+
+

ff0ac34c05f7c63980cb308ccdda4545f45be919
Mandrijauskaite <97032997+Mandrijauskaite@users.noreply.github.com>
2022-01-31 19:14:29 +0200

    Pataisytas juodrastis

--- js/01-uzduotis.js
+++ js/01-uzduotis.js
@@ -1,29 +1,60 @@
 console.log('------------');
-// Palyginti du skaičius a ir b. Išvesti į konsolę, kuris didesnis arba jie lygūs. (4 taškai)
+// 1. Palyginti du skaičius a ir b. Išvesti į konsolę, kuris didesnis arba jie lygūs. (4 taškai)
 
-const a = 3;
-const b = 5;
+// if (a >= b) {
+//     console.log('daugiau arba lygu');
+// } else {
+//     console.log('nei daugiau, nei lygu -> maziau');
+// }
+console.log('------------');
 
-if (a <= b) {
-console.log('daugiau arba lygu');
-} else {
-    console.log('nei daugiau, nei lygu -> maziau');
+// 2. Naudojant ciklą, išvesti į konsolę skaičius nuo 1 iki 10. (5 taškai)
+
+for (let i = 1; i < 11; i++) {
+    console.log(i);
 }
 console.log('------------');
-// Naudojant ciklą, išvesti į konsolę skaičius nuo 1 iki 10. (5 taškai)
-var skaiciai = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(skaiciai.length);
 
-console.log('------------');
-// Naudojant ciklą, išvesti į konsolę skaičius nuo 0, 2, 4, 6, 8, 10. (5 taškai)
-for (let i = 0; skaiciai.length - 1) {
-    console.log(skaiciai);
+// 3. Naudojant ciklą, išvesti į konsolę skaičius nuo 0, 2, 4, 6, 8, 10. (5 taškai)
+
+for (let i=0; i < 11; i = i + 2) {
+    console.log(i);
 }
 
-// Naudojant ciklą, sugeneruoti penkis atsitiktinius skaičius nuo 1 iki 10. Išvesti juos konsolėje. (5 taškai)
-// Naudojant ciklą, spausdinti atsitiktinius skaičius nuo 1 iki 10. Paskutinis atspausdintas skaičius turi būti 5. (7 taškai)
-// Sukurti masyvą, kurio ilgis būtų nuo 20 iki 30, o reikšmės būtų skaičiai nuo 10 iki 30. Surasti antrą didžiausią masyvo reikšmę, nenaudojant sort funkcijos. (7 taškai)
-// Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 100. Suskaičiuokite kiek yra kiekvienos raidės. (7 taškai)
+console.log('------------');
+
+// 4. Naudojant ciklą, sugeneruoti penkis atsitiktinius skaičius nuo 1 iki 10. Išvesti juos konsolėje. (5 taškai)
+
+for (let i=0; i < 5; i++) {
+    console.log(Math.floor(Math.random()*10)+1);
+}
+
+console.log('------------');
+
+// 5. Naudojant ciklą, spausdinti atsitiktinius skaičius nuo 1 iki 10. Paskutinis atspausdintas skaičius turi būti 5. (7 taškai)
+
+let skaicius;
+while (skaicius !== 5) {
+    skaicius = Math.floor(Math.floor(Math.random()*10)+1);
+    console.log(skaicius);
+}
+
+console.log('------------');
+
+// 6. Sukurti masyvą, kurio ilgis būtų nuo 20 iki 30, o reikšmės būtų skaičiai nuo 10 iki 30. Surasti antrą didžiausią masyvo reikšmę, nenaudojant sort funkcijos. (7 taškai)
+
+// let array = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 30, 31, 31];
+let array = [];
+let ilgis = Math.floor()
+let didziausia = 0;
+let antraDidziausia = 0;
+for (let i = 0; i < array.length; i++) {
+    if (array[i] > didziausia) {
+        antraDidziausia = didziausia;
+        didziausia = array[i];
+    }
+}
+// 7. Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 100. Suskaičiuokite kiek yra kiekvienos raidės. (7 taškai)
 // Parašyti funkciją - lygineSuma. Funkcijos parametrai - du kintamieji. Testų reikalavimai - abu kitamieji turi būti arba skaičiai arba masyvai(negali būti vienas skaičius, kitas masyvas).
 // Jei kintamieji skaičiai, grąžinti skaičių sumą, jei kintamieji masyvai - grąžinti masyvų ilgių sumą. Jei suma nelyginė - grąžinti tekstą, kad suma nelyginė. (10 taškų)
 // Parašyti funkciją pirminisSkaicius. Funkcija turi vieną kintamąjį. Turi būti patikrinimas, kad kintamasis yra skaičius. Funkcija turi grąžinti ar pateiktas skaičius yra pirminis( pirminis skaičius yra tas, kuris dalinasi tik iš savęs ir tik iš vieneto be liekanos.) (10 taškų)
